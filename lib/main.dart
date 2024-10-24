f1b525be57ec5225edfdd52cdb92b020a0d860ec
abhaysagarofficial <lcb2023037@iiitl.ac.in>
2024-10-24 23:17:51 +0530

    Fixing intialisation of firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,26 @@
+import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'customWidgets/photoBooth.dart';
 
-
-void main() async{
-
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  if(kIsWeb){
+  // Initialize Firebase for web and mobile platforms
+  if (kIsWeb) {
     await Firebase.initializeApp(
-        options: const FirebaseOptions(
-            apiKey: "AIzaSyDp988jFxJDrBlelw7q3kfh3wHR5rTOib0",
-            authDomain: "imagegram-356ca.firebaseapp.com",
-            projectId: "imagegram-356ca",
-            storageBucket: "imagegram-356ca.appspot.com",
-            messagingSenderId: "621545575805",
-            appId: "1:621545575805:web:2781c1c46a080e50ea6814",
-            measurementId: "G-5S24Q41002"
-        ));
-  }
-  else{
-    await Firebase.initializeApp();
+      options: FirebaseOptions(
+        apiKey: "AIzaSyDp988jFxJDrBlelw7q3kfh3wHR5rTOib0",
+        authDomain: "imagegram-356ca.firebaseapp.com",
+        projectId: "imagegram-356ca",
+        storageBucket: "imagegram-356ca.appspot.com",
+        messagingSenderId: "621545575805",
+        appId: "1:621545575805:web:2781c1c46a080e50ea6814",
+        measurementId: "G-5S24Q41002",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp(); // This automatically uses GoogleService-Info.plist for iOS
   }
 
   runApp(const MyApp());
@@ -33,7 +32,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Mytitle',
+      title: 'MyTitle',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
@@ -59,15 +58,14 @@ class MyHomePage extends StatelessWidget {
                 borderRadius: BorderRadius.circular(20),
                 child: Icon(Icons.account_circle_outlined, color: Colors.black),
               ),
-            )
+            ),
           ],
         ),
       ),
       body: ListView(
         children: [
-          // Horizontal List
           Container(
-            height: 150,  // Set a fixed height for the horizontal list container
+            height: 150,
             color: Colors.grey[200],
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
@@ -78,22 +76,17 @@ class MyHomePage extends StatelessWidget {
                   width: 100,
                   height: 100,
                   child: ClipRRect(
-                    child: Icon(
-                      Icons.account_circle_sharp,
-                      size: 100,
-                    ),
+                    child: Icon(Icons.account_circle_sharp, size: 100),
                   ),
                 );
               },
             ),
           ),
-
-          // Vertical List below
           Container(
             color: Colors.grey[200],
             child: ListView.builder(
-              physics: NeverScrollableScrollPhysics(), // Disable scrolling for this internal list
-              shrinkWrap: true, // Allow the ListView to occupy only as much space as needed
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
               itemCount: 15,
               itemBuilder: (BuildContext context, int index) {
                 return Photobooth();
